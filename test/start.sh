edf23760e0efd66125cc30b0b38eb1403b510145
VityaSchel <59040542+VityaSchel@users.noreply.github.com>
2025-09-06 20:40:04 +0200

    Refactor start.sh script

--- test/start.sh
+++ test/start.sh
@@ -8,41 +8,30 @@ configs = ""
 
 configs += " -f ../backend/docker-compose.yml"
 
+add_service_configs() {
+  local service=$1
+  configs += " -f ../docker-compose.${service}.yml"
+  if [ "$test" = "test" ]; then
+    configs += " -f ./docker-compose.override.${service}-test.yml"
+    configs += " --env-file ./test.${service}.env"
+  fi
+}
+
 if [ "$storage" = "s3" ]; then
-  configs += " -f ../docker-compose.s3.yml"
-  if [ "$test" = "test" ]; then
-    configs += " -f ./docker-compose.override.s3-test.yml"
-    configs += " --env-file ./test.s3.env"
-  fi
+  add_service_configs "s3"
 elif [ "$storage" = "fs" ]; then
-  configs += " -f ../docker-compose.fs.yml"
-  if [ "$test" = "test" ]; then
-    configs += " -f ./docker-compose.override.fs-test.yml"
-    configs += " --env-file ./test.fs.env"
-  fi
+  add_service_configs "fs"
 else
   echo "Error: Invalid storage type. Use 's3' or 'fs'." >&2
   exit 1
 fi
 
 if [ "$db" = "mongo" ]; then
-  configs += " -f ../docker-compose.mongodb.yml"
-  if [ "$test" = "test" ]; then
-    configs += " -f ./docker-compose.override.mongodb-test.yml"
-    configs += " --env-file ./test.mongodb.env"
-  fi
+  add_service_configs "mongodb"
 elif [ "$db" = "postgres" ]; then
-  configs += " -f ../docker-compose.postgresql.yml"
-  if [ "$test" = "test" ]; then
-    configs += " -f ./docker-compose.override.postgresql-test.yml"
-    configs += " --env-file ./test.postgresql.env"
-  fi
+  add_service_configs "postgresql"
 elif [ "$db" = "sqlite" ]; then
-  configs += " -f ../docker-compose.sqlite.yml"
-  if [ "$test" = "test" ]; then
-    configs += " -f ./docker-compose.override.sqlite-test.yml"
-    configs += " --env-file ./test.sqlite.env"
-  fi
+  add_service_configs "sqlite"
 else
   echo "Error: Invalid database type. Use 'mongo', 'postgres' or 'sqlite'." >&2
   exit 1
